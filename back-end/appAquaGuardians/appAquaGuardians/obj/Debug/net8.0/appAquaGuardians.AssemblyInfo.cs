45e27f18b304ed5580ea606a43bf1318a97d81e7
di0rio <caua.diorio29@gmail.com>
2024-09-05 19:42:33 -0300

    visualstudio

--- back-end/appAquaGuardians/appAquaGuardians/obj/Debug/net8.0/appAquaGuardians.AssemblyInfo.cs
+++ back-end/appAquaGuardians/appAquaGuardians/obj/Debug/net8.0/appAquaGuardians.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appAquaGuardians")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9942409b0b394fb01d21b2ca94a7587fc4a3486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4409a0a4e475c9619e2564a34e2a3320a9a4d6c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("appAquaGuardians")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appAquaGuardians")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

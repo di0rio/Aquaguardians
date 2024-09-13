d8970f68901af222d4b8c3e219965664dd7bef7c
di0riocaua <caua.diorio29@gmail.com>
2024-09-13 08:36:02 -0300

    resolvendo import Footer para Vercel

--- front-end/Sistema/Client/src/pages/App.jsx
+++ front-end/Sistema/Client/src/pages/App.jsx
@@ -1,7 +1,7 @@
-import { Outlet } from "react-router-dom";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 import Header from "../Components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import Footer from "../Components/Footer/Footer";
 
 const App = () => {
   return (

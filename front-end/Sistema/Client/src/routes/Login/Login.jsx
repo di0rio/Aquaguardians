e96ce4aaf6acf18ef7c1dfd141bf4d0462d70d28
di0riocaua <caua.diorio29@gmail.com>
2024-09-20 15:28:36 -0300

    login finalizado com rotas

--- front-end/Sistema/Client/src/routes/Login/Login.jsx
+++ front-end/Sistema/Client/src/routes/Login/Login.jsx
@@ -4,20 +4,40 @@ import { Link } from "react-router-dom";
 
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const navigation = [{ componente: "/users", name: "Usuários" }];
+const navigation = [{ componente: "/users", name: "Login" }];
 
 const Login = () => {
   return (
     <div className={styles.Container}>
-      {navigation.map((nav) => (
-        <Link
-          className={styles.Btn}
-          key={nav.name}
-          to={nav.componente}
-        >
-          {nav.name}
-        </Link>
-      ))}
+      <div className={styles.Conteudo}>
+        <div className={styles.Title}>
+          <h2>Login</h2>
+        </div>
+        <div className={styles.Inputs}>
+          <div className={styles.Email}>
+            <input type="email" placeholder="E-mail" />
+          </div>
+          <div className={styles.Password}>
+            <input type="password" placeholder="Senha" />
+          </div>
+        </div>
+        <div className={styles.Duvida}>
+          <a href="#">esqueceu a senha?</a>
+        </div>
+      </div>
+      <div className={styles.divBtn}>
+        <div className={styles.Btn}>
+          {navigation.map((nav) => (
+            <Link
+              className="nav-link nav-item py-1 py-md-none px-md-3 text-light fs-5"
+              key={nav.name}
+              to={nav.componente}
+            >
+              {nav.name}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };

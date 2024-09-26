403bc6614765614a47ef1061b351dd508d358d83
gustavo-lara <gustavolara63719@gmail.com>
2024-09-26 10:41:45 -0300

    Ajustando login, estilo e responsividade;

--- front-end/Sistema/Client/src/routes/Login/Login.jsx
+++ front-end/Sistema/Client/src/routes/Login/Login.jsx
@@ -8,34 +8,36 @@ const navigation = [{ componente: "/users", name: "Login" }];
 
 const Login = () => {
   return (
-    <div className={styles.Container}>
-      <div className={styles.Conteudo}>
-        <div className={styles.Title}>
-          <h2>Login</h2>
-        </div>
-        <div className={styles.Inputs}>
-          <div className={styles.Email}>
-            <input type="email" placeholder="E-mail" />
+    <div className={styles.Page}>
+      <div className={styles.Container}>
+        <div className={styles.Conteudo}>
+          <div className={styles.Title}>
+            <h2>LOGIN</h2>
           </div>
-          <div className={styles.Password}>
-            <input type="password" placeholder="Senha" />
+          <div className={styles.Inputs}>
+            <div className={styles.Email}>
+              <input type="email" placeholder="E-mail" />
+            </div>
+            <div className={styles.Password}>
+              <input type="password" placeholder="Senha" />
+            </div>
+          </div>
+          <div className={styles.Duvida}>
+            <a href="#">Esqueceu a senha?</a>
           </div>
         </div>
-        <div className={styles.Duvida}>
-          <a href="#">esqueceu a senha?</a>
-        </div>
-      </div>
-      <div>
-        <div className={styles.Btn}>
-          {navigation.map((nav) => (
-            <Link
-              className="nav-link nav-item py-1 py-md-none px-md-3 text-light fs-5"
-              key={nav.name}
-              to={nav.componente}
-            >
-              <button className={styles.divBtn}>{nav.name}</button>
-            </Link>
-          ))}
+        <div>
+          <div className={styles.Btn}>
+            {navigation.map((nav) => (
+              <Link
+                className="nav-link nav-item py-1 py-md-none px-md-3 text-light fs-5"
+                key={nav.name}
+                to={nav.componente}
+              >
+                <button className={styles.divBtn}>{nav.name}</button>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>

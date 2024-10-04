348d0fe91b13cfe4e2b0d5aa78bc97f2f04f4eb4
Felipe Salmazo <felipesalmazo123@gmail.com>
2024-10-04 09:21:35 -0300

    funcionarios finalizado

--- front-end/Sistema/Client/src/main.jsx
+++ front-end/Sistema/Client/src/main.jsx
@@ -8,11 +8,16 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Usuarios from "./routes/Usuarios/Usuarios.jsx";
 import Robos from "./routes/Robos/Robos.jsx";
 import Postos from "./routes/Postos/Postos.jsx";
+import Funcionarios from "./routes/Funcionarios/Funcionarios.jsx"
 import Login from "./routes/Login/Login.jsx";
+
 import CreatePosto from "./routes/CreatePosto/CreatePosto.jsx";
 import CreateRobot from "./routes/CreateRobot/CreateRobot.jsx";
+import CreateFuncionario from "./routes/CreateFuncionarios/CreateFuncionarios.jsx";
+
 import EditPosto from "./routes/EditPosto/EditPosto.jsx";
 import EditRobot from "./routes/EditRobot/EditRobot.jsx";
+import EditFuncionario from "./routes/EditFuncionario/EditFuncionario.jsx"
 
 
 const router = createBrowserRouter([  
@@ -25,10 +30,17 @@ const router = createBrowserRouter([
       { path: "users", element: <Usuarios /> },
       { path: "robos", element: <Robos /> },
       { path: "postos", element: <Postos /> },
+      { path: "funcionarios", element: <Funcionarios /> },
+      { path: "createfuncionario", element: <CreateFuncionario /> },
       { path: "createposto", element: <CreatePosto /> },
       { path: "createrobot", element: <CreateRobot /> },
       { path: "editposto", element: <EditPosto /> },
       { path: "editrobot", element: <EditRobot /> },
+      { path: "editfuncionario", element: <EditFuncionario /> },
+
+ 
+      
+
     ],
   },
 ]);

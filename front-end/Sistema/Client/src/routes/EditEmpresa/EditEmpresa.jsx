37a4e5b1211c6a6035e53ddc33a1109b5ab3ebda
N4rdelli <nardellianelize20@gmail.com>
2024-10-10 16:41:42 -0300

    Padronização de estilos em "Edit Empresa" Arquivos alterados: EditEmpresa.jsx | EditEmpresa.module.css

--- front-end/Sistema/Client/src/routes/EditEmpresa/EditEmpresa.jsx
+++ front-end/Sistema/Client/src/routes/EditEmpresa/EditEmpresa.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "./EditEmpresa.module.css"; // Estilos específicos para este componente
+import ButtonSubmit from "../../Components/ButtonSubmit/ButtonSubmit";
 
 const EditEmpresa = () => {
   const location = useLocation();
@@ -72,66 +73,70 @@ const EditEmpresa = () => {
     return <div>{error}</div>;
   }
 
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className={styles.container}>
-      <h1>Editar Empresa</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nome:</label>
-          <input
-            type="text"
-            name="name"
-            value={empresa.name}
-            onChange={handleChange}
-            required
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <div className={styles.header}>
+        <h2> Cadstrar Funcionário </h2>
+        <div className={styles.iconVoltar}>
+          <ion-icon
+            name="arrow-back-outline"
+            type="button"
+            onClick={handleGoBack}
           />
         </div>
-        <div>
-          <label>Nome para Contato:</label>
-          <input
-            type="text"
-            name="contactName"
-            value={empresa.contactName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Email para Contato:</label>
-          <input
-            type="email"
-            name="contactEmail"
-            value={empresa.contactEmail}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Telefone para Contato:</label>
-          <input
-            type="tel"
-            name="contactPhone"
-            value={empresa.contactPhone}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Endereço:</label>
-          <input
-            type="text"
-            name="address"
-            value={empresa.address}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" disabled={loading}>
+      </div>
+      <hr />
+      <div className={styles.container}>
+        <input
+          type="text"
+          name="name"
+          value={empresa.name}
+          onChange={handleChange}
+          required
+          placeholder="Nome"
+        />
+        <input
+          type="text"
+          name="contactName"
+          value={empresa.contactName}
+          onChange={handleChange}
+          required
+          placeholder="Nome para Contato"
+        />
+        <input
+          type="email"
+          name="contactEmail"
+          value={empresa.contactEmail}
+          onChange={handleChange}
+          required
+          placeholder="Email Para Contato"
+        />
+        <input
+          type="tel"
+          name="contactPhone"
+          value={empresa.contactPhone}
+          onChange={handleChange}
+          required
+          placeholder="Telefone Para Contato"
+        />
+        <input
+          type="text"
+          name="address"
+          value={empresa.address}
+          onChange={handleChange}
+          required
+          placeholder="Endereço"
+        />
+        <ButtonSubmit text="Editar Empresa" disabled={loading}>
           {loading ? "Atualizando..." : "Salvar Alterações"}
-        </button>
+        </ButtonSubmit>
         {error && <div className={styles.error}>{error}</div>}
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 

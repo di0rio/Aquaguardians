d31614119e19af5df80b8fcff24164b7e3a2b412
Felipe Salmazo <felipesalmazo123@gmail.com>
2024-10-17 14:01:48 -0300

    Dark/Light Mode feitos com sucesso

--- front-end/Sistema/Client/src/Components/ConfigPerfil/ConfigPerfil.jsx
+++ front-end/Sistema/Client/src/Components/ConfigPerfil/ConfigPerfil.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import { useAuth } from "../../Auth";
 import styles from "./ConfigPerfil.module.css";
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const ConfigPerfil = () => {
   const { logout } = useAuth();
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Altera o tema do body baseado no estado darkMode
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("bg-dark", "text-white");
+      document.body.classList.remove("bg-light", "text-dark");
+    } else {
+      document.body.classList.add("bg-light", "text-dark");
+      document.body.classList.remove("bg-dark", "text-white");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <div className={styles.container}>
       <div className="dropstart">
@@ -25,9 +42,12 @@ const ConfigPerfil = () => {
             />
           </li>
           <li>
-            <a className="dropdown-item" href="#">
-              Dark/Light Mode
-            </a>
+            <button
+              className="dropdown-item"
+              onClick={toggleDarkMode}
+            >
+              {darkMode ? "Light Mode" : "Dark Mode"}
+            </button>
           </li>
           <li>
             <button className="dropdown-item text-bg-danger" onClick={logout}>
--- front-end/Sistema/Client/src/Components/Header/Header.jsx
+++ front-end/Sistema/Client/src/Components/Header/Header.jsx
@@ -1,97 +1,97 @@
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+  import { Link } from "react-router-dom";
+  import { useLocation } from "react-router-dom";
 
-import styles from "./Header.module.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import ConfigPerfil from "../ConfigPerfil/ConfigPerfil";
+  import styles from "./Header.module.css";
+  import "bootstrap/dist/js/bootstrap.bundle.min.js";
+  import ConfigPerfil from "../ConfigPerfil/ConfigPerfil";
 
-const navigation = [
-  { componente: "/users", name: "Usuários" },
-  { componente: "/funcionarios", name: "Funcionarios" },
-  { componente: "/produtos", name: "Produtos" },
-  { componente: "/robos", name: "Robôs" },
-  { componente: "/postos", name: "Postos" },
-  { componente: "/empresas", name: "Empresas" },
-  { componente: "/transiction", name: "Transações" },
-];
+  const navigation = [
+    { componente: "/users", name: "Usuários" },
+    { componente: "/funcionarios", name: "Funcionarios" },
+    { componente: "/produtos", name: "Produtos" },
+    { componente: "/robos", name: "Robôs" },
+    { componente: "/postos", name: "Postos" },
+    { componente: "/empresas", name: "Empresas" },
+    { componente: "/transiction", name: "Transações" },
+  ];
 
-const Header = () => {
-  const location = useLocation();
+  const Header = () => {
+    const location = useLocation();
 
-  if (location.pathname === "/") {
-    return null;
-  }
+    if (location.pathname === "/") {
+      return null;
+    }
 
-  return (
-    <div className={styles.container}>
-      <nav
-        className="navbar navbar-expand-md top-0 container-fluid px-5"
-        style={{ background: "#160B28" }}
-      >
-        {/* 
-            data-bs-toggle="collapse": ativa a função de Colapso do Bootstrap, que mostra ou esconde um elemento (data-bs-target)
-            data-bs-target="#navbarNav": referencia o elemento que vai ser mostrado ou colapsado
-          */}
-
-        {/* Logo do AquaGuardians */}
-        <a className={styles.aquaGuardians} href="/">
-          AquaGuardians
-        </a>
-
-        {/* Ícone NavBar no Mobile */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
+    return (
+      <div className={styles.container}>
+        <nav
+          className="navbar navbar-expand-md top-0 container-fluid px-5"
+          style={{ background: "#160B28" }}
         >
-          <i className="bi bi-justify"></i>
-        </button>
+          {/* 
+              data-bs-toggle="collapse": ativa a função de Colapso do Bootstrap, que mostra ou esconde um elemento (data-bs-target)
+              data-bs-target="#navbarNav": referencia o elemento que vai ser mostrado ou colapsado
+            */}
 
-        {/* Restante da NavBar*/}
-        <div className="offcanvas offcanvas-start" id="offcanvasNavbar">
-          {/* Header no Mobile */}
-          <div className="offcanvas-header px-0">
-            <h5 className="offcanvas-title me-5">Nome do Administrador</h5>
-            <ConfigPerfil />
-            <button
-              type="button"
-              className="btn-close ms-5 p-0"
-              data-bs-dismiss="offcanvas"
-            >
-              <i className="bi bi-x-lg" />
-            </button>
-          </div>
+          {/* Logo do AquaGuardians */}
+          <a className={styles.aquaGuardians} href="/">
+            AquaGuardians
+          </a>
 
-          <hr
-            className="my-0 mx-4 d-md-none"
-            style={{ backgroundColor: "#fff" }}
-          />
+          {/* Ícone NavBar no Mobile */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+          >
+            <i className="bi bi-justify"></i>
+          </button>
 
-          {/* Body no Mobile e Links do Nav */}
-          <div className="offcanvas-body nav-underline mx-3 d-md-inline-flex justify-content-center">
-            {navigation.map((nav) => (
-              <Link
-                className="nav-link nav-item my-3 my-md-none mx-md-3"
-                key={nav.name}
-                to={nav.componente}
+          {/* Restante da NavBar*/}
+          <div className="offcanvas offcanvas-start" id="offcanvasNavbar">
+            {/* Header no Mobile */}
+            <div className="offcanvas-header px-0">
+              <h5 className="offcanvas-title me-5">Nome do Administrador</h5>
+              <ConfigPerfil />
+              <button
+                type="button"
+                className="btn-close ms-5 p-0"
+                data-bs-dismiss="offcanvas"
               >
-                {nav.name}
-              </Link>
-            ))}
+                <i className="bi bi-x-lg" />
+              </button>
+            </div>
+
+            <hr
+              className="my-0 mx-4 d-md-none"
+              style={{ backgroundColor: "#fff" }}
+            />
+
+            {/* Body no Mobile e Links do Nav */}
+            <div className="offcanvas-body nav-underline mx-3 d-md-inline-flex justify-content-center">
+              {navigation.map((nav) => (
+                <Link
+                  className="nav-link nav-item my-3 my-md-none mx-md-3"
+                  key={nav.name}
+                  to={nav.componente}
+                >
+                  {nav.name}
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
 
-        {/* 
-            Dropdown na direita em desktop
-          */}
-        <div className="d-none d-md-inline-flex">
-          <h5 className="my-auto ms-5 me-3">Nome do Adiministrador</h5>
-          <ConfigPerfil className={styles.Perf} />
-        </div>
-      </nav>
-    </div>
-  );
-};
+          {/* 
+              Dropdown na direita em desktop
+            */}
+          <div className="d-none d-md-inline-flex">
+            <h5 className="my-auto ms-5 me-3">Nome do Adiministrador</h5>
+            <ConfigPerfil className={styles.Perf} />
+          </div>
+        </nav>
+      </div>
+    );
+  };
 
-export default Header;
+  export default Header;

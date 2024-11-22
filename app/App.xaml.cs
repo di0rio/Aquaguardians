4e7b04a72a0279368d65be422710e4606ad2a060
B4g4rini <pedro.bagarini2000@gmail.com>
2024-11-22 08:14:13 -0300

    Criada a página de login

--- app/App.xaml.cs
+++ app/App.xaml.cs
@@ -1,12 +1,13 @@
-﻿namespace AquaGuardians
+﻿using SeuProjeto;
+
+namespace AquaGuardians
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new LoginPage());
         }
     }
 }
---
+++ app/LoginPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AquaGuardians;
+using Microsoft.Maui.Controls;
+
+namespace SeuProjeto
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            // Exemplo de lógica para validação do login
+            bool loginValido = true; // Substitua com sua lógica real
+
+            if (loginValido)
+            {
+                // Navegar para a MainPage ou outra página
+                Application.Current.MainPage = new AppShell();
+            }
+            else
+            {
+                DisplayAlert("Erro", "E-mail ou senha inválidos!", "OK");
+            }
+        }
+    }
+}
+
